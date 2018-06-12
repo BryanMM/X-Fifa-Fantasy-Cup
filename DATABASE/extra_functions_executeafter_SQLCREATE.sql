87874c01f7b185d6356c3c82f60fc85ef867456d
IsaacNez <shak04995@gmail.com>
2018-06-12 12:23:27 -0600

    Change database script

--- DATABASE/extra_functions_executeafter_SQLCREATE.sql
+++ DATABASE/extra_functions_executeafter_SQLCREATE.sql
@@ -19,10 +19,10 @@ as begin
 	declare @f_date_create DATETIME;
 	set @f_date_create = dateadd(hour,-8,@f_date_create_utc);
 	EXEC sp_executesql N'insert into fanatic(fanatic_login,fanatic_name,fanatic_last_name,fanatic_email,fanatic_phone,fanatic_birth,fanatic_date_create,fanatic_password,fanatic_private_key,fanatic_active,fanatic_photo,fanatic_description) values (@f_login,@f_name,@f_last_name,@f_email,@f_phone,@f_birth,@f_date_create,@f_password,@f_privatekey,@f_active,@f_photo,@f_about);';
-	EXEC sp_executesql N'insert into userxinfo values(2,@f_country,@f_login);';
+	EXEC sp_executesql N'insert into userxinfo(user_type_id,country_id,fanatic_login) values(2,@f_country,@f_login);';
 	return @variable;
 end;
-
+Go;
 create or alter function insertadmin(
 	@a_username varchar(8),
 	@a_name varchar(30),
@@ -41,3 +41,26 @@ as begin
 	exec sp_executesql N'insert into adminxinfo(admin_username,user_type_id) values(@a_username,1);';
 	return @variable;
 end;
+go;
+create or alter function insertplayer(
+	@p_passport int,
+	@p_name varchar(30),
+	@p_lastname varchar(30),
+	@p_birthdate date,
+	@p_height int,
+	@p_weight int,
+	@p_team varchar(30),
+	@p_price int,
+	@p_active bit,
+	@p_photo image,
+	@p_position int,
+	@p_country int
+)
+returns int
+as begin
+	declare @variable int;
+	exec sp_executesql N'insert into player values(@p_passport, @p_name,@p_lastname,@p_birthdate,@p_height,@p_weight,@p_team,@p_price,@p_active,@p_photo);';
+	exec sp_executesql N'insert into playerxinfo(country_id,playerxposition_id,player_id) values(@p_country,@p_position,@p_passport);';
+	return @variable;
+end;
+go;
